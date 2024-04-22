3166bb354b347e36a327af4f31a27c3f305c33fa
Ronenii <ronen.glm@gmail.com>
2024-04-23 00:46:03 +0300

    - Initialized repo

---
+++ src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/PollManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.MTAPizza.Sympoll.pollmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MTAPizza/Sympoll/pollmanagementservice/PollManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MTAPizza.Sympoll.pollmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
