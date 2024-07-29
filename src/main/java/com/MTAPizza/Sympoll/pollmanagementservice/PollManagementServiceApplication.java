0ed0cc6e77e1140201cd1e7351c34ffc7afd1a91
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-07-29 11:24:47 +0300

    trying different CORS mapping, through the application class.

--- src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/PollManagementServiceApplication.java
+++ src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/PollManagementServiceApplication.java
@@ -1,9 +1,10 @@
 package com.MTAPizza.Sympoll.pollmanagementservice;
 
-import com.MTAPizza.Sympoll.pollmanagementservice.controller.ServiceController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PollManagementServiceApplication {
@@ -11,4 +12,13 @@ public class PollManagementServiceApplication {
 		SpringApplication.run(PollManagementServiceApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:8080"); // http://frontend.default.svc.cluster.local:8080
+			}
+		};
+	}
 }
