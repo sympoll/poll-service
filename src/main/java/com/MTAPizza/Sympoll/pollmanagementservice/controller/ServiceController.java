13d5cb75eccb53e41993c6db1e8153b6dea5f644
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-07-29 08:26:15 +0300

    Added log messages to the ServiceController for more details on the requests received.

--- src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/controller/ServiceController.java
+++ src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/controller/ServiceController.java
@@ -5,6 +5,7 @@ import com.MTAPizza.Sympoll.pollmanagementservice.dto.poll.PollCreateRequest;
 import com.MTAPizza.Sympoll.pollmanagementservice.dto.poll.PollResponse;
 import com.MTAPizza.Sympoll.pollmanagementservice.service.poll.PollService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,24 +15,30 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/poll")
 @RequiredArgsConstructor
+@Slf4j
 public class ServiceController {
     private final PollService pollService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PollResponse createPoll(@RequestBody PollCreateRequest pollCreateRequest){
+        log.info("Received request to create a poll");
+        log.debug("Poll received to create: {}", pollCreateRequest);
         return pollService.createPoll(pollCreateRequest);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<PollResponse> getAllPolls(){
+        log.info("Received request to retrieve all polls");
         return pollService.getAllPolls();
     }
 
     @GetMapping("/id")
     @ResponseStatus(HttpStatus.OK)
     public PollResponse getPollById(@RequestParam UUID pollId){
+        log.info("Received request to get poll by id: {}", pollId);
+        log.debug("Poll ID received to get: {}", pollId);
         return pollService.getPollById(pollId);
     }
 
@@ -39,12 +46,15 @@ public class ServiceController {
     @DeleteMapping
     @ResponseStatus(HttpStatus.OK)
     public UUID deletePoll(@RequestParam UUID pollId){
+        log.info("Received request to delete poll with id {}", pollId);
+        log.debug("Poll ID received to delete: {}", pollId);
         return pollService.deletePoll(pollId);
     }
 
     @GetMapping("/health")
     @ResponseStatus(HttpStatus.OK)
     public HealthResponse HealthCheck(){
+        log.info("Received health check request, returning OK");
         return new HealthResponse("Running", "Poll Management Service is up and running.");
     }
 }
