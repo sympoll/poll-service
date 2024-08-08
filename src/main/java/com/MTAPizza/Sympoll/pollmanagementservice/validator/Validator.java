2824e42031192a51d857f7afe8584e26382e603a
Roy <roytol12@gmail.com>
2024-08-08 11:56:11 +0300

    - Changed validation check on Poll object to PollCreateRequest - Added types attribute in maven test workflow file

--- src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/validator/Validator.java
+++ src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/validator/Validator.java
@@ -1,9 +1,12 @@
 package com.MTAPizza.Sympoll.pollmanagementservice.validator;
 
+import com.MTAPizza.Sympoll.pollmanagementservice.dto.poll.PollCreateRequest;
 import com.MTAPizza.Sympoll.pollmanagementservice.model.poll.Poll;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 /**
  * This class handles the validation of data received from the client.
@@ -11,21 +14,25 @@ import java.time.LocalDateTime;
  */
 @Slf4j
 public class Validator {
-    public static void validateNewPoll(Poll poll) throws IllegalArgumentException{
-        validateAllowedVotingItems(poll.getVotingItems().size(), poll);
-        validateDeadline(poll.getTimeCreated(), poll);
+    public static void validateNewPoll(PollCreateRequest poll) throws IllegalArgumentException{
+        validateAllowedVotingItems(poll.votingItems().size(), poll);
+        validateDeadline(LocalDateTime.now(), poll);
     }
 
-    private static void validateAllowedVotingItems(int nofVotingItems, Poll poll) {
-        if (poll.getNofAnswersAllowed() > nofVotingItems) {
-            log.warn("User {} tried to create a poll but an number of answers allowed was given.", poll.getCreatorId());
+    private static void validateAllowedVotingItems(int nofVotingItems, PollCreateRequest poll) {
+        if (poll.nofAnswersAllowed() > nofVotingItems) {
+            log.warn("User {} tried to create a poll but an number of answers allowed was given.", poll.creatorId());
             throw new IllegalArgumentException("Number of allowed answers is greater than number of available answers");
         }
     }
 
-    private static void validateDeadline(LocalDateTime timeCreated, Poll poll){
-        if (poll.getDeadline().isBefore(timeCreated)) {
-            log.warn("User {} tried to create a poll but an invalid deadline was given.", poll.getCreatorId());
+    private static void validateDeadline(LocalDateTime timeCreated, PollCreateRequest poll){
+        // Convert deadline string time stamp to LocalDateTime object
+        Instant instant = Instant.parse(poll.deadline());
+        LocalDateTime deadline = instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        if (deadline.isBefore(timeCreated)) {
+            log.warn("User {} tried to create a poll but an invalid deadline was given.", poll.creatorId());
             throw new IllegalArgumentException("A deadline cannot be earlier than the time a poll was created");
         }
     }
