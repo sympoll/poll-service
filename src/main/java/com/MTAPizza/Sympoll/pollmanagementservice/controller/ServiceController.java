5a2f40f1af9588d44af6c5ba1c68c98e4ba0b001
Ronen Gelmanovich <ronenge@mta.ac.il>
2024-08-27 18:27:55 +0300

    - Removed annotation and added origin

--- src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/controller/ServiceController.java
+++ src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/controller/ServiceController.java
@@ -23,7 +23,6 @@ import java.util.UUID;
 @RequestMapping("/api/poll")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin
 public class ServiceController {
     private final PollService pollService;
     private final VotingItemService votingItemService;
