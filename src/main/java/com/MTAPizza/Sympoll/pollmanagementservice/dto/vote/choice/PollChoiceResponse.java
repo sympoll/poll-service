2965f081f5e37a1cf7cd168b7c9aafc643049368
Ronenii <ronen.glm@gmail.com>
2024-09-02 20:47:37 +0300

    - Adjusted tests to be able to work with vote client. -

--- src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/dto/vote/choice/PollChoiceResponse.java
+++ src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/dto/vote/choice/PollChoiceResponse.java
@@ -1,7 +1,6 @@
 package com.MTAPizza.Sympoll.pollmanagementservice.dto.vote.choice;
 
 import java.util.List;
-import java.util.UUID;
 
 public record PollChoiceResponse(List<Integer> votingItemIds) {
 }
