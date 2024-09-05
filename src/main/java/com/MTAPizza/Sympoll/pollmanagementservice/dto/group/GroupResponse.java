bb4c9e5527414d0f4b9a378de10cd23ad46b5f00
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-09-05 04:11:34 +0300

    Poll Now receiving full data of the group instead of just their names.

--- src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/dto/group/GroupResponse.java
+++ src/main/java/com/MTAPizza/Sympoll/pollmanagementservice/dto/group/GroupResponse.java
@@ -3,6 +3,6 @@ package com.MTAPizza.Sympoll.pollmanagementservice.dto.group;
 public record GroupResponse (
         String groupId,
         String groupName,
-        String groupProfilePictureUrl
+        String profilePictureUrl
 ) {
 }
